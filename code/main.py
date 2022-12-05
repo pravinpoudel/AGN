average accuracy calculation and k -fold validation removed back to fix number

code/main.py
@@ -50,17 +50,14 @@ modelsOption = {
 }
 
 def retriveData(dataDir):
-    global myDirName
     myDirName = os.path.join(DATASET_DIR, dataDir, "train.data") 
     # the schema is stored in the config file inside the directory
     # We don't need to explicitly call the close() method. It is done internally.
     with open(os.path.join(myDirName, "config.yml"), "r") as stream:
         try:
             conf_data = yaml.safe_load(stream)
-            # print(conf_data)
             print("---------------------------------------configuration file opened successfully ------------------------------------------------")
         except yaml.YAMLError as exc:
-            print("there is an error")
             print(exc)
     
     # read tsv file and get pandas.DataFrame
@@ -115,7 +112,7 @@ def retriveData(dataDir):
 
     # load train label and test lable
     try: 
-        # test_labels = pd.read_csv(os.path.join(myDirName, 'test.csv'), sep=_seperator, header=0)
+        test_labels = pd.read_csv(os.path.join(myDirName, 'test.csv'), sep=_seperator, header=0)
         train_labels = pd.read_csv(os.path.join(myDirName, 'train.csv'), sep=_seperator, header=0)
     except IOError as err:
         print(err)
@@ -128,17 +125,18 @@ def retriveData(dataDir):
     test_mask = np.zeros(features.shape[0],  dtype=bool)
     validation_mask = np.random.rand(features.shape[0]) < ratio_VD
     _class = [];
-    # for index, row in test_labels.iterrows():
-    #     node1, label1 = row.tolist() 
-    #     labels[node1] = label1
-    #     test_mask[node1] = 1
-    #     _class.append(label1) if label1 not in _class else _class
+    for index, row in test_labels.iterrows():
+        node1, label1 = row.tolist() 
+        labels[node1] = label1
+        test_mask[node1] = 1
+        _class.append(label1) if label1 not in _class else _class
 
     # Main Logic here is: https://stackoverflow.com/questions/2451386/what-does-the-caret-operator-do
     assert False == True^True
     # we need to make false in the validation mask which is test data so False = True * False
     validation_mask *= train_mask #now this is our Final validation mask
     # since we got validation mask from training data we need to update that mask as well 
+    train_mask = train_mask ^ validation_mask
 
     for index, row in train_labels.iterrows():
         node2, label2 = row.tolist()
@@ -150,7 +148,8 @@ def retriveData(dataDir):
     print("number of labels - ", len(_class))
 
 
-    return conf_data, edge_index, edges_attr, features, labels, train_mask, test_mask
+    return conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask, test_mask
+
 
 def retriveTestData(trainIndices, testIndices, feature, label, conf_data):
     print("traing feature array is")
@@ -241,33 +240,30 @@ def main(args):
     loadedFile = os.path.join(DATASET_DIR, args.dataset, "load.pt")
     if not os.path.exists(loadedFile):
         print("data loading from source")
-        conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask = retriveData(args.dataset)
-        torch.save([conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask], loadedFile)
+        conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask, test_mask = retriveData(args.dataset)
+        torch.save([conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask, test_mask], loadedFile)
     else:
         print("loaded from the loaded file")
-        conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask = torch.load(loadedFile)
+        conf_data, edge_index, edges_attr, features, labels, train_mask, validation_mask, test_mask = torch.load(loadedFile)
+
+    data = Data(x=features, edge_index=edge_index, edge_attr=edges_attr, y=labels, train_mask = train_mask, validation_mask = validation_mask, test_mask = test_mask  )
+    data = data.to(device)
 
     # https://pytorch-geometric.readthedocs.io/en/latest/modules/data.html
     # create homogeneous graph: Awesome tool
     #TODO: Watch more videos in pytorch-geometric
     # in documentation: https://pytorch-geometric.readthedocs.io/en/1.3.2/modules/data.html shape [num_nodes, num_node_features] mean shape should be this value
-    # we already have that shape; don't get confused with [] it just mean tuple of shape tensor.shape()
-
-    
-    
-    model = modelsOption["SAGE"](conf_data["class_count"], conf_data["feature_count"], hidden_layers=64, drop_out_rate = 0.6)
+    # we already have that shape; don't get confused with [] it just mean tuple of shape tensor.shape()    
+    model = modelsOption["SAGE"](conf_data["class_count"], conf_data["feature_count"], hidden_layers=64, drop_out_rate = 0.5)
         # print("your model is created successfully")
         # print("sorry ", sys.exc_info()[0], "happened")
 
     # https://www.youtube.com/watch?v=7q7E91pHoW4&ab_channel=PythonEngineer
     # loss_fn = nn.CrossEntropyLoss() 
     # create optimizer
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.005, weight_decay=0.0001)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=0.001)
     # https://towardsdatascience.com/epoch-vs-iterations-vs-batch-size-4dfb9c7ce9c9
-    numb_epoch = 1000
-    k_folds = 5
-    kfold = KFold(n_splits=k_folds, shuffle=True)
-
+    numb_epoch = 200
     model.resetParameter()
     model.train()
     eval_maxAccuracy = 0
@@ -277,73 +273,46 @@ def main(args):
     # As the number of epochs increases, more number of times the weight are changed in the neural network and the curve goes from underfitting to optimal to overfitting curve.
     start = time.time()
     max_eval_epoch = -1.0
-    fraction = 1/k_folds
-    seg = int(conf_data["n_node"]*fraction)
-    print("hello")
-    for k_step in range(k_folds):
-        print("------------------------------------")
-        print(f'FOLD {k_step}')
-        print("------------------------------------")
-        
-        traingingStart = 0
-        trainingEnd = k_step*seg
-        validatiionStart = trainingEnd
-        validationEnd = k_step*seg + seg
-        trainingSecStart = validationEnd
-        trainingSecEnd = conf_data["n_node"]-1
 
-        train_left_indices = list(range(traingingStart,trainingEnd))
-        train_right_indices = list(range(trainingSecStart,trainingSecEnd))
-                
-        train_indices = train_left_indices + train_right_indices
-        val_indices = list(range(validatiionStart,validationEnd))
-
-        train_mask, testMask, trainFeature, trainLable, testFeature, testLabel, edge_indexTrain, edges_attrTrain, edge_indexTest, edges_attrTest = retriveTestData(train_indices, val_indices, features, labels, conf_data)
-
-
-
-        trainData = Data(x=trainFeature, edge_index=edge_index, edge_attr=edges_attr, y=trainLable, train_mask = train_mask, validation_mask = validation_mask, test_mask = testMask  )
-        testData = Data(x=testFeature, edge_index=edge_index, edge_attr=edges_attr, y=testLabel, train_mask = train_mask, validation_mask = validation_mask, test_mask = testMask  )
-
-        # print(" i am main data", data)
-        # print(data.validation_mask)
-        # learned that after y it is same as rest parameter in JS and Data is nothing just a dictionary
-        # https://pytorch.org/docs/stable/generated/torch.Tensor.to.html
-        # creates an another array with data as an element and "device:cpu" as second element
-        trainData = trainData.to(device)
-        testData = testData.to(device)
-        totalSize = trainData.size(dim=0)
-        for epoch in range(numb_epoch):
-            optimizer.zero_grad()
-            print("training")        
-            outgraph = model(trainData)
+    averageTrainingAccuracy = 0
+    averageTestingAccuracy = 0        
+    for epoch in range(numb_epoch):
+        optimizer.zero_grad()
+        print("training")        
+        trainOut = model(data)
             # print("training accuracy")
-            loss = F.nll_loss(outgraph[trainData.train_mask], trainData.y[trainData.train_mask])
-            train_acc = accuracy_calculation(outgraph[trainData.train_mask], trainData.y[trainData.train_mask])
-            if train_acc > train_maxAccuracy:
-                    train_maxAccuracy = train_acc
-            # print(train_acc)
-            loss.backward()
-            optimizer.step() 
+        loss = F.nll_loss(trainOut[data.train_mask], data.y[data.train_mask])
+        train_acc = accuracy_calculation(trainOut[data.train_mask], data.y[data.train_mask])
+        averageTrainingAccuracy += train_acc
+        if train_acc > train_maxAccuracy:
+            train_maxAccuracy = train_acc
+        loss.backward()
+        optimizer.step() 
 
             # set flag to disable grad calculation because you don't want to change parameter
             #  and weight on testing and validation
         
-            with torch.no_grad():
-                model.eval()
-                print("testing")
-                testOut = model(testData)
-                evaluation_accuracy = accuracy_calculation(testOut[testData.test_mask], testData.y[testData.test_mask])
-                if evaluation_accuracy > eval_maxAccuracy:
-                    eval_maxAccuracy = evaluation_accuracy
-                    max_eval_epoch = epoch
-                elif epoch - max_eval_epoch >= 100: 
-                    break
-                model.train()
+        with torch.no_grad():
+            model.eval()
+            print("testing")
+            testOut = model(data)
+            evaluation_accuracy = accuracy_calculation(testOut[data.test_mask], data.y[data.test_mask])
+            averageTestingAccuracy += evaluation_accuracy
+            if evaluation_accuracy > eval_maxAccuracy:
+                eval_maxAccuracy = evaluation_accuracy
+                max_eval_epoch = epoch
+            # -----------------------------------------------------------------------
+            # this is for early stopping and most popular value to check is 100 for 1000 epochs
+            # i.e. epoch - max_eval_epoch>=100 but here i am comparing the average accuracy and is only on 200 epoch so it does not matter much so i am commentng; you can uncomment the else if condition 
+            # -----------------------------------------------------------------------
+            # elif epoch - max_eval_epoch >= 50: 
+                # break
+            model.train()
         # summary(model=model, input_size=(1, conf_data["feature_count"], 64, 64, conf_data["class_count"]))  
     end = time.time()
-    print("training accuracy: ", train_maxAccuracy)
-    print("testing accuracy: ", eval_maxAccuracy)
+    print("average training accuracy: ", averageTrainingAccuracy/numb_epoch)
+    print("average testing accuracy: ", averageTestingAccuracy/numb_epoch)
+
     elapsed_time = end - start
     print("Training time is:", elapsed_time, "seconds")
 